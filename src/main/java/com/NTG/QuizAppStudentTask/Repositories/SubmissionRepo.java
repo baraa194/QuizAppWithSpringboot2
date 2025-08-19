b42a705ec86121a5b4dc771b70ce1ff8fc490b75
baraa194 <149835134+baraa194@users.noreply.github.com>
2025-08-19 12:38:40 +0300

    Add files via upload

---
+++ src/main/java/com/NTG/QuizAppStudentTask/Repositories/SubmissionRepo.java
@@ -0,0 +1,24 @@
+package com.NTG.QuizAppStudentTask.Repositories;
+
+import com.NTG.QuizAppStudentTask.Models.Quiz;
+import com.NTG.QuizAppStudentTask.Models.Submission;
+import com.NTG.QuizAppStudentTask.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SubmissionRepo extends JpaRepository<Submission,Integer> {
+    boolean existsByStudentAndQuiz(User student, Quiz quiz);
+
+    @Query("SELECT s FROM Submission s WHERE s.deadline <= :now AND s.submittedAt IS NULL")
+    List<Submission> findExpiredUnsubmitted(@Param("now") LocalDateTime now);
+
+
+    Optional<Submission> findByStudentAndQuiz(User student, Quiz quiz);
+}
